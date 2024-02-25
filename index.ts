ab23fcaf6751d70d20280da1035b5bc71b3ece12
wTait <>
2024-02-25 12:31:06 -0800

    add ascii art

--- index.ts
+++ index.ts
@@ -1,3 +1,23 @@
 #! /usr/bin/env bun
+import { $, file } from "bun"
 
-console.log("Hello via Bun!");
+// Function to create a print delay
+function delay(time: number) {
+  return new Promise(resolve => setTimeout(resolve, time))
+}
+
+async function printArt(filePath: string) {
+  // Read the file content
+  let content = await file(filePath).text()
+  
+  // Split the content into lines
+  let lines = content.split('\n')
+  
+  // Iterate over each line
+  for (let line of lines) {
+    console.log(line)
+    await delay(75)
+  }
+}
+
+await printArt('art/bucli.txt')
